d0af659f072e35badc8052f0f4e5dbf38e96a987
peace0096 <peace0096@naver.com>
2023-11-05 02:19:24 +0900

    패킷 자동화 도입 전, 선언된 enum(Haeder ID)에 따른 패킷 핸들러 호출 추가

--- GameServer/ServerPacketHandler.cpp
+++ GameServer/ServerPacketHandler.cpp
@@ -1,24 +1,18 @@
 #include "pch.h"
 #include "ServerPacketHandler.h"
-#include "BufferReader.h"
-#include "BufferWriter.h"
 
-void ServerPacketHandler::HandlePacket(BYTE* buffer, int32 len)
+PacketHandlerFunc GPacketHandler[UINT16_MAX];
+
+// ÄÁÅÙÃ÷ ÀÛ¾÷ ÇÊ¿ä
+
+bool Handle_INVALID(PacketSessionRef& session, BYTE* buffer, int32 len)
 {
-	BufferReader br(buffer, len);
-
-	PacketHeader header;
-	br.Peek(&header);
-
-	switch (header.id)
-	{
-	default:
-		break;
-
-	}
+	PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+	// TODO : Log
+	return true;
 }
 
-SendBufferRef ServerPacketHandler::MakeSendBuffer(Protocol::S_TEST& pkt)
+bool Handle_S_TEST(PacketSessionRef& session, Protocol::S_TEST& pkt)
 {
-	return _MakeSendBuffer(pkt, S_TEST);
+	return true;
 }
